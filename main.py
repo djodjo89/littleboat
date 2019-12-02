7283a2d90fdaf4050d46281dfe64d839d552f773
Djodjo <mathysimion@yahoo.fr>
2019-12-02 07:54:48 +0100

    LB-2 feat/prog(userInterface): allow user to enter x parameter for p2 control point

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov 29 23:18:19 2019
-
 @author: danidani
 """
 
@@ -55,5 +54,62 @@ def display_bezier_quad(matControlPoint):
             )
     matPoint = matPointLine.T
     display_point(matPoint)
-    return matPoint             
-                
+    return matPoint
+
+"""
+Creation of the method for bezier curve
+"""
+
+def visu_point(matPoint,style):
+    # matPoint contains points coordinates
+    x = matPoint[0, :]
+    y = matPoint[1, :]
+    plt.plot(x, y, style)
+
+
+def visu_BezierQuad(matPointControl,str):
+    
+    n=50
+    mt = np.linspace(0,1.,n)  
+    matt = np.ones((3,n))  # only many 1
+    matt[1,:] = mt  # line with many t
+    matt[2,:] = mt*mt  # line with many t*t
+
+    matBezier3 = np.array([[1, 0, 0], 
+                           [-2, 2, 0], 
+                           [1, -2, 1]])
+
+    matPointligne = np.dot(np.dot(matt.T,matBezier3),matPointControl.T)
+    #Transposition
+    matPoint=matPointligne.T
+
+    #Draw point of the curve
+    visu_point(matPointControl,'b.')
+    
+    #Draw dot between extremities and control point
+    #visu_point(matPointControl,'b:')
+    
+    #Draw segments
+    visu_point(matPoint,str)
+    
+# For testing purposes only
+def askXParameter():
+    try:
+        x = int(input("Saisissez l'abscisse du point de contrôle: "))
+        return x
+    except ValueError:
+        print("Veuillez saisir un entier")
+        return askXParameter()
+
+
+"""
+Creation of the sail
+"""
+#Sail without rotation or wind 
+#P1 = (0,0), P2 = (0, 3), P3 = (0, 6)
+P2x = askXParameter()
+P2y = 3
+matPointControl = np.array([[0,P2x,0],[0,P2y,6]])
+visu_BezierQuad(matPointControl, 'b') 
+
+
